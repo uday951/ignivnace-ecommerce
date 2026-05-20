8b9ce60c40e92d0cf32f6099807d49d7775b6ef9
Uday915 <udaydd6062@gmail.com>
2026-05-20 15:30:55 +0530

    api changed

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://ignivnace-ecommerce-ctrs.vercel.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
